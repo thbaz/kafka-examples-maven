7d00b2c24ed71928ab9bc041333b5032a3c028b6
thbaz <thiago.bassani@gmail.com>
2017-08-09 04:40:27 +0200

    test suit

---
+++ src/test/java/ch/scigility/kafka/StreamProducerTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class StreamProducerTest {
+    @Test
+    public void evaluateOther() {
+        assertEquals(1, 1);
+    }
+    //@Before
+    //@After
+    //@BeforeClass
+    //@AfterClass
+}
