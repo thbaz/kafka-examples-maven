ab95fd7c206a8dd8527cba4c1db8c8b7376d5d03
thbaz <thiago.bassani@gmail.com>
2017-08-09 06:04:45 +0200

    thread consumer

--- src/test/java/ch/scigility/kafka/StreamProducerTest.java
+++ src/test/java/ch/scigility/kafka/StreamProducerTest.java
@@ -1,10 +1,39 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import ch.scigility.kafka.Consumer;
+import java.io.IOException;
 
 public class StreamProducerTest {
     @Test
     public void evaluateOther() {
-        assertEquals(1, 1);
+
+
+          Thread thread = new Thread(new Runnable() {
+              @Override
+              public void run() {
+                  System.out.println("Running");
+                  try {
+                    Consumer.main();
+                  } catch (IOException e) {
+                      // Do something here
+                  }
+              }
+          });
+          thread.start();
+          int runFor = 10*1000;
+          int sleepFor = 1000;
+          long endTimeMillis = System.currentTimeMillis() + runFor;
+          while (thread.isAlive()) {
+              if (System.currentTimeMillis() > endTimeMillis) {
+                  System.out.println("The End");
+                  break;
+              }
+              try {
+                  System.out.println("Running: "+((endTimeMillis - System.currentTimeMillis())/1000) +" s");
+                  Thread.sleep(sleepFor);
+              }
+              catch (InterruptedException t) {}
+          }
     }
     //@Before
     //@After
